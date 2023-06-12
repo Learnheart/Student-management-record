287fec9545e1456045886216c7fad46dd04ef1f1
NguyenThanhTrung2306 <103129624+NguyenThanhTrung2306@users.noreply.github.com>
2023-06-12 12:15:51 +0700

    Course.java

--- src/main/java/org/example/Model/Course.java
+++ src/main/java/org/example/Model/Course.java
@@ -1,8 +1,38 @@
-package org.example.Model;
+package org.example.model;
 
 public class Course {
     private int courseId;
     private String courseName;
     private String score;
-    
+
+    public Course(int courseId, String courseName, String score) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.score = score;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
 }
+
