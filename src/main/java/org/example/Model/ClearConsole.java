fda70e2abc4384f9209bf12995031f216ddacce0
NguyenThanhTrung2306 <103129624+NguyenThanhTrung2306@users.noreply.github.com>
2023-06-12 20:55:47 +0700

    ClearConsole.java

---
+++ src/main/java/org/example/Model/ClearConsole.java
@@ -0,0 +1,24 @@
+public class ConsoleUtils {
+
+    public static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is some text.");
+        clearConsole();
+        System.out.println("Console cleared!");
+    }
+}
